0c73ca179da5e6a218b7af61a4e377e35ed0f8bb
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-07-26 14:46:09 -0300

    init

---
+++ client/js/index.js
@@ -0,0 +1,10 @@
+var campos = [
+  document.querySelector('#data'),
+  document.querySelector('#quantidade'),
+  document.querySelector('#valor')
+];
+
+document.querySelector('.form').addEventListener('submit',function(event) {
+   
+
+})
