2c07034b903d81515ba93239e549bad7d6e8d891
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-07-27 05:11:56 -0300

    Create NegociacaoController.js

---
+++ client/js/app/controller/NegociacaoController.js
@@ -0,0 +1,25 @@
+class  NegociacaoController {
+
+
+  constructor() {
+    let $ = document.querySelector.bind(document);
+    this._inputData = $('#data');
+    this._inputQuantidade = $('#quantidade');
+    this._inputValor = $('#valor');
+
+  }
+
+  adiciona(event){
+
+    event.preventDefault();
+
+    let negociacao = new Negociacao(
+      this._inputData.value,
+      this._inputQuantidade.value,
+      this._inputValor.value
+    );
+
+    console.log(negociacao);
+   
+  }
+}
