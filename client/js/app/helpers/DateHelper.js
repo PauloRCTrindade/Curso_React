26d472d1f4957809fdb9418d7bddaf862b2f1b8c
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-07-28 05:26:18 -0300

    Update DateHelper.js

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,14 +1,17 @@
 class DateHelper {
-  textoParaData(texto){
+
+  constructor (){
+    throw new Error('Esta classe não pode ser instaciada')
+  }
+
+  static textoParaData(texto) {
     return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
 
   }
 
-  dataParaTexto(data){
+  static dataParaTexto(data) {
 
-    return data.getDate()
-     + '/' + (data.getMonth() + 1)
-     + '/' + data.getFullYear();
+    return ` ${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
 
   }
 }
